ad71e5b7c98bea87c5b8da3cded1c2c303eb3596
Dmitry <dmytro.oborskiy@angleto.com>
2024-11-20 19:20:52 +0200

    update(Works): Skyrocket animation

--- src/components/(Home)/Works/Skyrocket.jsx
+++ src/components/(Home)/Works/Skyrocket.jsx
@@ -1,14 +1,58 @@
 import HoverLink from '@/components/HoverLink';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import Image from 'next/image';
 import React, { useRef } from 'react';
 
 const Skyrocket = ({ styles }) => {
+  const container = useRef();
+  const imageRef = useRef();
   const mediaRef = useRef();
 
+  useGSAP(
+    () => {
+      const words = container.current.querySelectorAll('[data-animation]');
+      const each = 0.05;
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: container.current,
+            start: 'top 50%',
+            end: 'bottom bottom',
+            markers: true,
+          },
+        })
+        .add(() => mediaRef.current.play())
+        .from(container.current, {
+          opacity: 0,
+          duration: 0.5,
+        })
+        .to(gsap.utils.toArray([imageRef.current, mediaRef.current]), {
+          clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0px 100%)',
+          duration: 1.5,
+          ease: 'power4.inOut',
+        })
+        .from(
+          words,
+          {
+            duration: 0.1,
+            opacity: 0,
+            stagger: {
+              each,
+              grid: 'auto',
+              from: 'random',
+            },
+          },
+          '-=0.5'
+        );
+    },
+    { dependencies: [] }
+  );
+
   return (
-    <div className={styles.skyrocket}>
+    <div className={styles.skyrocket} ref={container}>
       <div className={styles.media}>
-        <div className={styles.media__logo}>
+        <div className={styles.media__logo} ref={imageRef}>
           <Image
             src="/img/logo_sky.svg"
             width="82"
@@ -22,7 +66,6 @@ const Skyrocket = ({ styles }) => {
             width="1280"
             height="769"
             preload="auto"
-            autoPlay
             muted
             loop
             playsInline
@@ -30,10 +73,21 @@ const Skyrocket = ({ styles }) => {
             <source src="/video/works/Skyrocket_preview.mp4" type="video/mp4" />
           </video>
           <div className={styles.information}>
-            <h3 className={styles.information__title}>Skyrocket</h3>
+            <h3 className={styles.information__title}>
+              {Array.from('Skyrocket').map((l, i) => (
+                <span data-animation key={`name-${l}-${i}-${l}`}>
+                  {l}
+                </span>
+              ))}
+            </h3>
             <p className={styles.information__description}>
-              Brand, Web and Motion Design for the Digital <br /> Agency
-              SKYROCKET
+              {Array.from(
+                'Brand, Web and Motion Design for the Digital Agency SKYROCKET'
+              ).map((l, i) => (
+                <span data-animation key={`name-${l}-${i}-${l}`}>
+                  {l}
+                </span>
+              ))}
             </p>
             <div className={styles.information__link}>
               <HoverLink href="/works/koenigsegg">View Case</HoverLink>
