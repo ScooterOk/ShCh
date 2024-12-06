d06a625585cb791533a5675f82d8dad66bdd4243
Dmitry <dmytro.oborskiy@angleto.com>
2024-12-07 01:08:44 +0200

    update(Hero): update title model

--- src/components/(Home)/HomeHeroTitle.jsx
+++ src/components/(Home)/HomeHeroTitle.jsx
@@ -7,6 +7,12 @@ import gsap from 'gsap';
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 
+import configs from '@/configs/titlesAnimation';
+
+const { duration, easeEnter, easeLeave } = configs;
+
+const animatingNodes = {};
+
 const HomeHeroTitle = () => {
   const { isLoaded, setNoScroll } = useContext(mainContext);
   const [widthScale, setWidthScale] = useState(1);
@@ -17,24 +23,11 @@ const HomeHeroTitle = () => {
     depth: 0,
   });
 
-  // const { value } = useControls({
-  //   lerpLookAt: folder(
-  //     {
-  //       value: { value: 0, label: "value", min: 0, max: 2 },
-  //     },
-  //     { collapsed: true }
-  //   ),
-  // });
-
   const action = useRef(null);
 
-  const model = useGLTF('/models/Home_digital_3dtext_full.gltf');
+  const model = useGLTF('/models/Home_digital_3dtext_02_test.gltf');
 
-  const { scene, animations } = model;
-
-  //   const gltf = useLoader(GLTFLoader, "/models/stacy.glb", "", (loader) => {
-  //     console.log("loader", loader);
-  //   });
+  const { scene, animations, nodes } = model;
 
   const { actions, ref, names } = useAnimations(animations);
 
@@ -96,17 +89,331 @@ const HomeHeroTitle = () => {
     setWidthScale(w);
   }, [modelDimensions.width, viewport.width]);
 
-  // console.log('Viewport width:', viewportWidth);
-  // console.log('Model width in viewport:', modelWidthInViewport);
-  // console.log('size:', size);
+  const handlePointerEnter = (e) => {
+    const target = e.eventObject.morphTargetInfluences;
+    const name = e.eventObject.name;
+    if (animatingNodes?.[name]?.isActive()) animatingNodes?.[name].kill();
+    animatingNodes[name] = gsap.to(target, {
+      [0]: 1,
+      duration,
+      ease: easeEnter,
+      overwrite: true,
+    });
+  };
+
+  const handlePointerLeave = (e) => {
+    const target = e.eventObject.morphTargetInfluences;
+    const name = e.eventObject.name;
+    if (!animatingNodes?.[name]?.isActive()) {
+      console.log('LEAVE');
+      gsap.to(target, {
+        [0]: 0,
+        duration,
+        ease: 'power2.inOut',
+        overwrite: 'auto',
+      });
+    } else {
+      animatingNodes?.[name].eventCallback('onComplete', () => {
+        gsap.to(target, {
+          [0]: 0,
+          duration,
+          ease: easeLeave,
+        });
+      });
+    }
+  };
 
   return (
-    <primitive
-      ref={ref}
-      object={scene}
-      position={[0.045, 0, 0]}
-      scale={widthScale}
-    />
+    // <primitive
+    //   ref={ref}
+    //   object={scene}
+    //   position={[0.045, 0, 0]}
+    //   scale={widthScale}
+    // />
+    <group ref={ref} dispose={null} position={[0, 0, 0]} scale={widthScale}>
+      <group>
+        <group name="Null1">
+          <group name="Null">
+            <mesh
+              name="E"
+              castShadow
+              receiveShadow
+              geometry={nodes.E.geometry}
+              material={nodes.E.material}
+              morphTargetDictionary={nodes.E.morphTargetDictionary}
+              morphTargetInfluences={nodes.E.morphTargetInfluences}
+              position={[-0.268, 0.105, 0.042]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="C"
+              castShadow
+              receiveShadow
+              geometry={nodes.C.geometry}
+              material={nodes.C.material}
+              morphTargetDictionary={nodes.C.morphTargetDictionary}
+              morphTargetInfluences={nodes.C.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="N"
+              castShadow
+              receiveShadow
+              geometry={nodes.N.geometry}
+              material={nodes.N.material}
+              morphTargetDictionary={nodes.N.morphTargetDictionary}
+              morphTargetInfluences={nodes.N.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="E_1"
+              castShadow
+              receiveShadow
+              geometry={nodes.E_1.geometry}
+              material={nodes.E_1.material}
+              morphTargetDictionary={nodes.E_1.morphTargetDictionary}
+              morphTargetInfluences={nodes.E_1.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="I"
+              castShadow
+              receiveShadow
+              geometry={nodes.I.geometry}
+              material={nodes.I.material}
+              morphTargetDictionary={nodes.I.morphTargetDictionary}
+              morphTargetInfluences={nodes.I.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="R"
+              castShadow
+              receiveShadow
+              geometry={nodes.R.geometry}
+              material={nodes.R.material}
+              morphTargetDictionary={nodes.R.morphTargetDictionary}
+              morphTargetInfluences={nodes.R.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="E_2"
+              castShadow
+              receiveShadow
+              geometry={nodes.E_2.geometry}
+              material={nodes.E_2.material}
+              morphTargetDictionary={nodes.E_2.morphTargetDictionary}
+              morphTargetInfluences={nodes.E_2.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="P"
+              castShadow
+              receiveShadow
+              geometry={nodes.P.geometry}
+              material={nodes.P.material}
+              morphTargetDictionary={nodes.P.morphTargetDictionary}
+              morphTargetInfluences={nodes.P.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="X"
+              castShadow
+              receiveShadow
+              geometry={nodes.X.geometry}
+              material={nodes.X.material}
+              morphTargetDictionary={nodes.X.morphTargetDictionary}
+              morphTargetInfluences={nodes.X.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="E_3"
+              castShadow
+              receiveShadow
+              geometry={nodes.E_3.geometry}
+              material={nodes.E_3.material}
+              morphTargetDictionary={nodes.E_3.morphTargetDictionary}
+              morphTargetInfluences={nodes.E_3.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="N_1"
+              castShadow
+              receiveShadow
+              geometry={nodes.N_1.geometry}
+              material={nodes.N_1.material}
+              morphTargetDictionary={nodes.N_1.morphTargetDictionary}
+              morphTargetInfluences={nodes.N_1.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="G"
+              castShadow
+              receiveShadow
+              geometry={nodes.G.geometry}
+              material={nodes.G.material}
+              morphTargetDictionary={nodes.G.morphTargetDictionary}
+              morphTargetInfluences={nodes.G.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="I_1"
+              castShadow
+              receiveShadow
+              geometry={nodes.I_1.geometry}
+              material={nodes.I_1.material}
+              morphTargetDictionary={nodes.I_1.morphTargetDictionary}
+              morphTargetInfluences={nodes.I_1.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="S"
+              castShadow
+              receiveShadow
+              geometry={nodes.S.geometry}
+              material={nodes.S.material}
+              morphTargetDictionary={nodes.S.morphTargetDictionary}
+              morphTargetInfluences={nodes.S.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="E_4"
+              castShadow
+              receiveShadow
+              geometry={nodes.E_4.geometry}
+              material={nodes.E_4.material}
+              morphTargetDictionary={nodes.E_4.morphTargetDictionary}
+              morphTargetInfluences={nodes.E_4.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="D"
+              castShadow
+              receiveShadow
+              geometry={nodes.D.geometry}
+              material={nodes.D.material}
+              morphTargetDictionary={nodes.D.morphTargetDictionary}
+              morphTargetInfluences={nodes.D.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="L"
+              castShadow
+              receiveShadow
+              geometry={nodes.L.geometry}
+              material={nodes.L.material}
+              morphTargetDictionary={nodes.L.morphTargetDictionary}
+              morphTargetInfluences={nodes.L.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="A"
+              castShadow
+              receiveShadow
+              geometry={nodes.A.geometry}
+              material={nodes.A.material}
+              morphTargetDictionary={nodes.A.morphTargetDictionary}
+              morphTargetInfluences={nodes.A.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="T"
+              castShadow
+              receiveShadow
+              geometry={nodes.T.geometry}
+              material={nodes.T.material}
+              morphTargetDictionary={nodes.T.morphTargetDictionary}
+              morphTargetInfluences={nodes.T.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="I_2"
+              castShadow
+              receiveShadow
+              geometry={nodes.I_2.geometry}
+              material={nodes.I_2.material}
+              morphTargetDictionary={nodes.I_2.morphTargetDictionary}
+              morphTargetInfluences={nodes.I_2.morphTargetInfluences}
+              position={[-0.226, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="G_1"
+              castShadow
+              receiveShadow
+              geometry={nodes.G_1.geometry}
+              material={nodes.G_1.material}
+              morphTargetDictionary={nodes.G_1.morphTargetDictionary}
+              morphTargetInfluences={nodes.G_1.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="I_3"
+              castShadow
+              receiveShadow
+              geometry={nodes.I_3.geometry}
+              material={nodes.I_3.material}
+              morphTargetDictionary={nodes.I_3.morphTargetDictionary}
+              morphTargetInfluences={nodes.I_3.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+            <mesh
+              name="D_1"
+              castShadow
+              receiveShadow
+              geometry={nodes.D_1.geometry}
+              material={nodes.D_1.material}
+              morphTargetDictionary={nodes.D_1.morphTargetDictionary}
+              morphTargetInfluences={nodes.D_1.morphTargetInfluences}
+              position={[-0.268, 0.105, 0]}
+              onPointerEnter={handlePointerEnter}
+              onPointerLeave={handlePointerLeave}
+            />
+          </group>
+        </group>
+      </group>
+    </group>
   );
 };
 
---
+++ src/configs/titlesAnimation.js
@@ -0,0 +1,5 @@
+export default {
+  duration: 0.7,
+  easeEnter: 'power2.inOut',
+  easeLeave: 'power2.inOut',
+};
