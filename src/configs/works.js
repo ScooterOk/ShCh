cb96c2459a6d5752093fdc9b85fc76aba6ef62a3
Dmitry <dmytro.oborskiy@angleto.com>
2025-03-05 22:18:33 +0200

    feat(Configs): Works details

---
+++ src/configs/works.js
@@ -0,0 +1,12 @@
+export const heavensake = {
+  hero: {
+    title: 'HEAVENSAKE',
+    description:
+      'HEAVENSAKE is a luxury sake brand that embodies a unique combination of Japanese tradition and French artistry, helmed by the acclaimed French cellar master Régis Camus.',
+    tags: ['Brand Design'],
+  },
+  credits: [
+    { name: 'Brand Design', title: 'Serhii Churilov' },
+    { name: 'Brand Strategy', title: 'Vasyl Artiushchenko' },
+  ],
+};
